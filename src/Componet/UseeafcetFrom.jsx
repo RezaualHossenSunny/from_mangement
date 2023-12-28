caeacd948767be86eb0cd5b2d497fa5995aeb098
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2023-12-29 00:26:45 +0600

    input from add

--- src/Componet/UseeafcetFrom.jsx
+++ src/Componet/UseeafcetFrom.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 const UseeafcetFrom = () => {
   return (
     <div>
-
+        <from>
+            <input placeholder='enter first name'/> <br/>
+            <input placeholder='enter last name'/> <br/>
+<select>
+    <option value="">chose your city</option>
+    <option value="Dhaka">Dhaka</option>
+    <option value="chitagong">chitagong</option>
+</select>
+<br/>
+<input type='radio'/> male             
+<input type='radio'/> Female 
+<br/>
+<button type='submit'> submit </button>           
+        </from>
     </div>
   )
 }
