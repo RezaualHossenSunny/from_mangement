55d692ef16970bcc9409ffc14853a9adbd0ef956
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2023-12-29 00:21:28 +0600

    from manement usefcet

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import UseeafcetFrom from './Componet/UseeafcetFrom'
+
+function App() {
+ 
+
+  return (
+    <>
+<UseeafcetFrom></UseeafcetFrom>
+    </>
+  )
+}
+
+export default App
---
+++ src/Componet/UseeafcetFrom.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UseeafcetFrom = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default UseeafcetFrom
